9b3af75209421ab05252f96ed1ae710f767d4ddf
Anderzon-1989 <richardanderzonchavez@gmail.com>
2025-04-25 15:41:57 -0500

    archivos html,css y js de contactenos

---
+++ Arteria/js/contactenos.js
@@ -0,0 +1,32 @@
+
+  // formspree //
+  var form = document.getElementById("my-form");
+
+  async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+          method: form.method,
+          body: data,
+          headers: {
+              'Accept': 'application/json'
+          }
+      }).then(response => {
+          if (response.ok) {
+              status.innerHTML = "Gracias por habernos contactado.";
+              form.reset()
+          } else {
+              response.json().then(data => {
+                  if (Object.hasOwn(data, 'errors')) {
+                      status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+                  } else {
+                      status.innerHTML = "¡Uy! Hubo un problema al enviar tu formulario."
+                  }
+              })
+          }
+      }).catch(error => {
+          status.innerHTML = "¡Uy! Hubo un problema al enviar tu formulario."
+      });
+  }
+  form.addEventListener("submit", handleSubmit)
