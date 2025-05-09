bc2b32e5dd9c36cbb94f8b9165c8472bcc5dbf15
Aleja955 <alejapaiva97@gmail.com>
2025-05-09 13:10:53 -0500

    Boton de Explorar funcional junto con los filtros

--- Arteria/js/navbar-profile-menu.js
+++ Arteria/js/navbar-profile-menu.js
@@ -22,26 +22,26 @@ export function initProfileMenu (loggedIn = true) {
       <li><a href="../html/Registro.html">Registrate</a></li>
     </ul>
     `;
-  
+
     let isLoggedIn = loggedIn;
-  
+
     // Botón y menú existen porque el header ya se insertó
     const profileBtn  = document.getElementById('hdrProfileBtn');
     const profileMenu = document.getElementById('hdrProfileMenu');
-  
+
     profileBtn.addEventListener('click', e => {
       e.stopPropagation();
       profileMenu.innerHTML = isLoggedIn ? menuLoggedIn : menuLoggedOut;
       const opened = profileMenu.classList.toggle('open');
       profileBtn.setAttribute('aria-expanded', opened);   // accesibilidad
     });
-  
+
     window.addEventListener('click', e => {
       if (!profileMenu.contains(e.target) && !profileBtn.contains(e.target)) {
         profileMenu.classList.remove('open');
       }
     });
-  
+
     return {
       setLoggedIn: status => { isLoggedIn = status; },
       getLoggedIn: () => isLoggedIn
